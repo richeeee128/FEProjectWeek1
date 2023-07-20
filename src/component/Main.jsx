274813e9d1e9bebf475dc280008b7b930b940b14
Chaeyeon Lee <96226559+richeeee128@users.noreply.github.com>
2023-07-20 09:22:00 +0900

    Revert "Hl"

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Search from "./Search";
-import List from "./List";
 
 function Main() {
   return (
     <div>
+      <h1>Main</h1>
       <Header />
       <Search />
-      <List />
     </div>
   );
 }
